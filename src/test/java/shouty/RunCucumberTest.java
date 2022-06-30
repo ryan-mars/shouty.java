a55d15e2f970f208742fbe4bd43647176569736e
Ryan Marsh <ryan@stochastic.dev>
2022-06-30 08:09:27 -0500

    JUnit Platform Engine/Suite, Gradle >5, Cucumber 7.2

--- src/test/java/shouty/RunCucumberTest.java
+++ src/test/java/shouty/RunCucumberTest.java
@@ -1,22 +1,18 @@
 package shouty;
 
-import io.cucumber.junit.CucumberOptions;
-import io.cucumber.junit.CucumberOptions.SnippetType;
-import io.cucumber.junit.Cucumber;
-import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        plugin = {
-                "pretty",
-                "html:target/cucumber",
-                "rerun:target/rerun.txt",
-                "json:target/cucumber.json"
-        },
-        snippets = SnippetType.CAMELCASE
-)
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("shouty")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "shouty")
 public class RunCucumberTest {
-  // this is the adapter/bridge code
-  // between cucumber jvm and junit
-  // you do not need to edit this
+        // this is the adapter/bridge code
+        // between cucumber jvm and junit
+        // you do not need to edit this
 }
--- src/test/java/shouty/ShoutSteps.java
+++ src/test/java/shouty/ShoutSteps.java
@@ -8,7 +8,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 import static java.util.Collections.emptyMap;
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class ShoutSteps {
